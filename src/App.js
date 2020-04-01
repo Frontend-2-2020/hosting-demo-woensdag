4a032851565d8ba90d83ca8a7a42b986132fc960
jannick vandaele <jannickvandaele@gmail.com>
2020-04-01 19:33:29 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <h1>Jannicks React Website</h1>
+        <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/about">
+              <h2>About</h2>
+            </Route>
+            <Route path="/">
+              <h2>Home</h2>
+            </Route>
+          </Switch>
+        </Router>
+    );
+  }
 }
 
 export default App;
